b8003b555b4422c602896626bf498aa480facaa8
nhatnv6767 <nhatnvse90183@gmail.com>
2022-02-28 22:03:16 +0900

    config rinkeby

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import '../styles/globals.css'
 import { ThirdwebWeb3Provider } from '@3rdweb/hooks'
 
+const supportedChainIds = [4]// rinkeby == 4
+const connectors = {
+  injected: {},
+}
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }

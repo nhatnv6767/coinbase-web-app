67f4043c7d08c44b81a5c497921a9f01223add3f
nhatnv6767 <nhatnvse90183@gmail.com>
2022-02-28 23:53:15 +0900

    styled

--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from 'styled-components'
 import Header from '../components/Header'
 
 const Dashboard = ({address}) => {
@@ -8,3 +9,15 @@ const Dashboard = ({address}) => {
 }
 
 export default Dashboard
+
+const Wrapper = styled.div`
+    display: flex;
+    height: 100vh;
+    width: 100vw;
+    background-color: #0a0b0d;
+    color: white;
+`
+
+const MainContainer = styled.div`
+    flex: 1;
+`

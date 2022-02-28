281208db1033ec83815ee2c887191288385a66bc
nhatnv6767 <nhatnvse90183@gmail.com>
2022-02-28 22:08:37 +0900

    yarn add styled components

--- pages/_app.js
+++ pages/_app.js
@@ -12,7 +12,7 @@ function MyApp({ Component, pageProps }) {
       supportedChainIds={supportedChainIds}
       connectors={connectors}
     >
-      return <Component { ... pageProps} />
+      <Component { ... pageProps} />
     </ThirdwebWeb3Provider>
   )
 }

5b8c5dfbe2142bc49c15908316689c7c74b99c12
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-01 00:18:42 +0900

    all the styled

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Portfolio = () => {
   return (
@@ -7,3 +8,47 @@ const Portfolio = () => {
 }
 
 export default Portfolio
+
+const Wrapper = styled.div`
+    flex: 1;
+    display: flex;
+    justify-content: center;
+`
+
+const Content = styled.div`
+    width: 100%;
+    max-width: 1000px;
+    padding: 2rem 1rem;
+`
+
+const PortfolioTable = styled.div`
+    margin-top: 1rem;
+    border: 1px solid #282b2f;
+`
+
+const Table = styled.table`
+    width: 100%;
+`
+
+const TableRow = styled.tr`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+
+    & > th {
+        text-align: left;
+    }
+`
+
+const TableItem = styled.div`
+    padding: 1rem 2rem;
+`
+// dau gach ngan cach 2 token
+const Divider = styled.div`
+    border-bottom: 1px solid #282b2f;
+`
+
+const Title = styled.div`
+    font-size: 1.5rem;
+    font-weight: 600;
+`

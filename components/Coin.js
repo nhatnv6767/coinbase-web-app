3f401b0ccae0327b13f5a4f2a90c18af1c526b03
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-01 21:36:41 +0900

    let coin.js

---
+++ components/Coin.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import Image from 'next/image'
+
+
+const Coin = () => {
+  return (
+    <div>Coin</div>
+  )
+}
+
+export default Coin

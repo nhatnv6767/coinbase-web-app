e70be33561a6152b75510c92fbf329659e644fae
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-01 21:29:38 +0900

    Formating

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -15,11 +15,13 @@ const Portfolio = () => {
                 <Table>
                     <TableItem>
                         <TableRow>
-                            <div>Name</div>
-                            <div>Balance</div>
-                            <div>Price</div>
-                            <div>Allocation</div>
-                            <div><BsThreeDotsVertical /></div>
+                            <div style={{ flex: 3 }}>Name</div>
+                            <div style={{ flex: 2 }}>Balance</div>
+                            <div style={{ flex: 1 }}>Price</div>
+                            <div style={{ flex: 1 }}>Allocation</div>
+                            <div style={{ flex: 0 }}>
+                                <BsThreeDotsVertical />
+                            </div>
                         </TableRow>
                     </TableItem>
                 </Table>

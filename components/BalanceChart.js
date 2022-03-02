4d01d1fc96baa47236ca0d04a41bf96bd6be8b8f
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-03 00:13:27 +0900

    Line config

--- components/BalanceChart.js
+++ components/BalanceChart.js
@@ -1,2 +1,15 @@
 import styled from "styled-components";
 import { Line } from 'react-chartjs-2'
+
+
+const BalanceChart = () => {
+  return (
+    <Wrapper>
+        <Line data={data} options={options} width={400} height={150} />
+    </Wrapper>
+  )
+}
+
+export default BalanceChart
+
+const Wrapper = styled.div``

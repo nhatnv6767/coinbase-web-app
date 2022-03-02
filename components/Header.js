7345ead76ac86bf0609eb42e40bb5bff308af4e7
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-02 23:09:27 +0900

    condition

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Header = () => {
+const Header = ({walletAddress, connectWallet}) => {
     return (
         <Wrapper>
             <Title>Assets</Title>

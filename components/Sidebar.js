6a6f8c5ace33186bac0f40f5f83722f00379a032
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-02 21:46:38 +0900

    Update Sidebar.js
    styled

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Sidebar = () => {
   return (
@@ -7,3 +8,20 @@ const Sidebar = () => {
 }
 
 export default Sidebar
+
+const Wrapper = styled.div`
+    height: calc(100vh);
+    border-right: 1px solid #282b2f;
+    width: calc(22rem - 16px - 16px);
+    padding: 0 1rem;
+`
+
+const LogoContainer = styled.div`
+    margin: 1.5rem 0;
+`
+
+const Logo = styled.div`
+    width: 44%;
+    object-fit: contain;
+    margin-left: 1.5rem;
+`

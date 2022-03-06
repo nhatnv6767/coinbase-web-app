8a72ed9bd4d2602001e716fe4d1e6cd719a28b57
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-07 00:44:25 +0900

    more style

--- components/modal/Transfer.js
+++ components/modal/Transfer.js
@@ -14,7 +14,7 @@ const Transfer = () => {
                         onChange={e => setAmount(e.target.value)} />
                     <span>ETH</span>
                 </FlexInputContainer>
-                <Warning style={{ color: '' && '#0a0b0d' }}>
+                <Warning style={{ color: amount && '#0a0b0d' }}>
                     Amount is a required field
                 </Warning>
             </Amount>
@@ -72,4 +72,85 @@ const Warning = styled.div`
     padding: 1rem 0 2rem 0;
     text-align: center;
     color: #8a919e;
+`
+
+const Divider = styled.div`
+    border-bottom: 1px solid #282b2f;
+`
+
+const Row = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    color: #8a919e;
+    padding: 1rem 0;
+    font-size: 1.2rem;
+`
+
+const FieldName = styled.div`
+    flex: 0.5;
+    padding-left: 2rem;
+`
+
+const Icon = styled.div`
+    margin-right: 1rem;
+    height: 1.8rem;
+    width: 1.8rem;
+    border-radius: 50%;
+    overflow: hidden;
+    display: grid;
+
+    & > img {
+        /* margin: -0.5rem 1rem; */
+        height: 120%;
+        width: 120%;
+        object-fit: cover;
+    }
+`
+// when type => style input
+const Recipient = styled.input`
+    flex: 1;
+    border: none;
+    background: none;
+    outline: none;
+    color: white;
+    font-size: 1.2rem;
+    text-wrap: wrap;
+    margin-right: 0.5rem;
+`
+
+const CoinSelectList = styled.div`
+    display: flex;
+    flex: 1.3;
+    height: 100%;
+
+    &:hover {
+        cursor: pointer;
+    }
+`
+
+const CoinName = styled.div`
+    flex: 1;
+    border: none;
+    background: none;
+    outline: none;
+    color: white;
+    font-size: 1.2rem;
+    text-wrap: wrap;
+    margin-right: 0.5rem;
+`
+
+const Continue = styled.button`
+    color: white;
+    width: 100%;
+    background-color: #3773f5;
+    padding: 1rem;
+    text-align: center;
+    border-radius: 0.4rem;
+    font-size: 1.2rem;
+
+    &:hover {
+        cursor: pointer;
+        background-color: #4a80f6;
+    }
 `

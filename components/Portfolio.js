505202a66261fed9a29eb727833438bc3dfce83a
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-06 12:29:51 +0900

    sdk module

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -27,7 +27,9 @@ const Portfolio = () => {
             const sanityTokens = (await coins.json()).result
             setSanityTokens(sanityTokens)
 
-            sanityTokens.map(token => console.log(token.contractAddress))
+            sanityTokens.map(token =>
+                console.log(sdk.getTokenModule(token.contractAddress))
+            )
         }
         return getSanityAndThirdWebTokens()
     }, [])

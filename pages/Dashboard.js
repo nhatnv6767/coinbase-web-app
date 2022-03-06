bce1d5e91ccb03439955b465984991dde391ce44
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-06 12:44:39 +0900

    some fix

--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import Header from '../components/Header'
 import Main from '../components/Main'

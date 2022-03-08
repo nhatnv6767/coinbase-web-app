0ebf3ff6555860029e5c65c6d24827cfcd07d30b
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-08 20:52:32 +0900

    icon some

--- components/modal/Transfer.js
+++ components/modal/Transfer.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
+import { FaWallet } from 'react-icons/fa'
 
 const Transfer = () => {
     const [amount, setAmount] = useState()
@@ -22,6 +23,13 @@ const Transfer = () => {
             <TransferForm>
                 <Row>
                     <FieldName>To</FieldName>
+                    <Icon>
+                        <FaWallet />
+                    </Icon>
+                    <Recipient
+                        placeholder='Address'
+                        
+                    />
                 </Row>
             </TransferForm>
         </Wrapper>

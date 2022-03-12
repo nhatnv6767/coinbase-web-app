32f6a376e8cafa260c82760cf0895f2a909286cb
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-12 17:19:18 +0900

    doneeee

--- components/modal/CoinItem.js
+++ components/modal/CoinItem.js
@@ -59,6 +59,9 @@ const CoinItem = ({
           <Symbol>{token.symbol}</Symbol>
         </NameDetails>
       </Main>
+      <Balance>
+        {balance} {token.symbol}
+      </Balance>
     </Wrapper>
   )
 }

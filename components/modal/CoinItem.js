e3a29d3227a7a8e031b2b8d5ebbf6c85e143b6b8
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-12 16:41:55 +0900

    styling

--- components/modal/CoinItem.js
+++ components/modal/CoinItem.js
@@ -22,3 +22,34 @@ const Main = styled.div`
     display: flex;
     align-items: center;
 `
+
+const Icon = styled.div`
+    margin-right: 1rem;
+    height: 1.8rem;
+    width: 1.8rem;
+    border-radius: 50%;
+    overflow: hidden;
+    display: grid;
+    place-items: center;
+
+    & > img {
+      height: 120%;
+      width: 120%;
+      object-fit: cover;
+    }
+`
+
+const NameDetails = styled.div``
+
+const Name = styled.div`
+  font-size: 1.1rem;
+  margin-bottom: 0.2rem;
+`
+
+const Balance = styled.div``
+
+const IsSelected = styled.div`
+  margin-left: 0.5rem;
+  color: #3773f5;
+`
+
